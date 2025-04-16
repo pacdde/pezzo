f9a1f852697ac7a3b5340690dc18cdd6642e5dd4
pacdde <199569106+pacdde@users.noreply.github.com>
2025-04-16 04:57:31 +0000

    fix(server): cors default to *

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.NX_CORS || true,
+    origin: process.env.NX_CORS || '*',
     credentials: true,
     allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
   });
